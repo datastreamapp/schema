338dfeb482709f387bab24748e1384c4cad7906c
will Farrell <will.farrell@gmail.com>
2018-02-26 16:28:05 -0700

    fix: add json-table to build

--- bin/build.js
+++ bin/build.js
@@ -11,7 +11,7 @@ console.log('Building JSON Schema & JSON Table Schema & CSV Template');
 const srcGlob = __dirname+'/../src/*.json';    // Note files starting w/ `definitions.` will be skipped in code
 const csvFile = __dirname+'/../dist/csv/template.csv';
 const jsonSchemaDir = __dirname+'/../dist/json-schema';
-//const jsonTableSchemaDir = __dirname+'/../dist/json-table-schema';
+const jsonTableSchemaDir = __dirname+'/../dist/json-table-schema';
 
 glob(srcGlob)
     .then((files) => {
@@ -23,21 +23,55 @@ glob(srcGlob)
 
             console.log('Processing:', file);
 
-
-
             const jsonSchemaFile = jsonSchemaDir + '/' + file;
-            //const jsonTableSchemaFile = jsonTableSchemaDir +'/' + file;
+            const jsonTableSchemaFile = jsonTableSchemaDir +'/' + file;
 
             const deref = $RefParser.dereference(filePath)
                 .then((schemaJSON) => {
                     //console.log(schemaFile, schemaJSON);
 
-                    let csv = `"`+Object.keys(schemaJSON.properties).join(`","`)+`"`+"\r\n";
+                    const columns = Object.keys(schemaJSON.properties);
+
+                    // csv
+                    let csv = `"`+columns.join(`","`)+`"`+"\r\n";
+
+                    // json-schema -> json-schema-table
+                    const table = {
+                        fields:[]
+                    };
+                    for(let i = 0, l = columns.length; i<l; i++) {
+                        const key = columns[i];
+                        const field = schemaJSON.properties[key];
+                        const column = {
+                            name:key,
+                            title:field.title,
+                            description:field.description,
+                            type:field.type,
+                            constraints:{
+                                required: (schemaJSON.required.indexOf(key) !== -1) ? true : null
+                            }
+                        };
+                        if (field.hasOwnProperty('format')) {
+                            column.format = field.format;
+                            if (field.format === 'date-time') {
+                                column.format = field.format.replace('-', '');
+                            }
+                        }
+
+                        const constraints = ['minLength','maxLength','minimum','maximum','pattern','enum'];
+                        for(let j = 0, m = constraints.length; j<m; j++) {
+                            if (field.hasOwnProperty(constraints[j])) {
+                                column.constraints[constraints[j]] = field[constraints[j]];
+                            }
+                        }
+
+                        table.fields.push(column);
+                    }
 
                     return Promise.all([
                         writeFile(csvFile, csv, {encoding:'utf8'}),
                         writeFile(jsonSchemaFile, JSON.stringify(schemaJSON, null, 2), {encoding:'utf8'}),
-                        //writeFile(jsonTableSchemaFile, JSON.stringify(defaultJSON, null, 2), {encoding:'utf8'})
+                        writeFile(jsonTableSchemaFile, JSON.stringify(table, null, 2), {encoding:'utf8'})
                     ]);
                 })
                 .catch((err) => {
