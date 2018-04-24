eb1efa6b56ba1b08a0649e23aa89e7a5d0eecc8c
will Farrell <will.farrell@gmail.com>
2018-04-24 16:56:36 -0600

    feat: add in enum transformations

--- bin/build.js
+++ bin/build.js
@@ -1,135 +1,135 @@
-const util = require('util');
-const path = require('path');
-const fs = require('fs');
+const util = require('util')
+const path = require('path')
+const fs = require('fs')
 
-const changeCase = require('change-case');
-const glob = util.promisify(require('glob'));
-const $RefParser = require('json-schema-ref-parser');
-const Ajv = require('ajv'); // version >= 4.7.4
-const pack = require('ajv-pack');
+const changeCase = require('change-case')
+const glob = util.promisify(require('glob'))
+const $RefParser = require('json-schema-ref-parser')
+const Ajv = require('ajv') // version >= 4.7.4
+const pack = require('ajv-pack')
 
 const ajv = new Ajv({
-    v5: true,
-    format: 'full',
-    coerceTypes: true,
-    allErrors: true,
-    useDefaults: true,
-    sourceCode: true    // this option is required
-});
-const writeFile = util.promisify(fs.writeFile);
+  v5: true,
+  format: 'full',
+  coerceTypes: true,
+  allErrors: true,
+  useDefaults: true,
+  sourceCode: true    // this option is required
+})
+require('ajv-keywords')(ajv, ['switch','transform'])
 
-console.log('Compile: JSON Schema & JSON Table Schema & CSV Template');
+const writeFile = util.promisify(fs.writeFile)
 
-const srcGlob = __dirname+'/../src/*.json';    // Note files starting w/ `definitions.` will be skipped in code
-const validateFile = __dirname+'/../dist/validate/index.js';
-const csvFile = __dirname+'/../dist/csv/template.csv';
-const sqlFile = __dirname+'/../dist/sql/index.sql';
-const jsonSchemaDir = __dirname+'/../dist/json-schema';
-const jsonTableSchemaDir = __dirname+'/../dist/json-table-schema';
+console.log('Compile: JSON Schema & JSON Table Schema & CSV Template')
 
-const repalce = (str) => {
-    // fix typos in wqx allowed values
-    return str
-        //.replace('ug/l', 'µg/l'); // opted to keep u
+const srcGlob = __dirname + '/../src/*.json'    // Note files starting w/ `definitions.` will be skipped in code
+const validateFile = __dirname + '/../dist/validate/index.js'
+const csvFile = __dirname + '/../dist/csv/template.csv'
+const sqlFile = __dirname + '/../dist/sql/index.sql'
+const jsonSchemaDir = __dirname + '/../dist/json-schema'
+const jsonTableSchemaDir = __dirname + '/../dist/json-table-schema'
+
+const replace = (str) => {
+  // fix typos in wqx allowed values
+  return str
+  //.replace('ug/l', 'µg/l'); // opted to keep u
 
 }
 
 glob(srcGlob)
-    .then((files) => {
-        const arr = [];
-        files.forEach((filePath) => {
-            const parts = path.parse(filePath);
-            const file = parts.base;
-            if (file.indexOf('definitions.') === 0) { return; }    // skip definitions files
+  .then((files) => {
+    const arr = []
+    files.forEach((filePath) => {
+      const parts = path.parse(filePath)
+      const file = parts.base
+      if (file.indexOf('definitions.') === 0) { return }    // skip definitions files
 
-            console.log('Processing:', file);
+      console.log('Processing:', file)
 
-            const jsonSchemaFile = jsonSchemaDir + '/' + file;
-            const jsonTableSchemaFile = jsonTableSchemaDir +'/' + file;
+      const jsonSchemaFile = jsonSchemaDir + '/' + file
+      const jsonTableSchemaFile = jsonTableSchemaDir + '/' + file
 
-            const deref = $RefParser.dereference(filePath)
-                .then((schemaJSON) => {
-                    //console.log(schemaFile, schemaJSON);
+      const deref = $RefParser.dereference(filePath)
+        .then((schemaJSON) => {
+          //console.log(schemaFile, schemaJSON);
 
-                    const columns = Object.keys(schemaJSON.properties);
+          const columns = Object.keys(schemaJSON.properties)
 
-                    // ## csv
-                    let csv = `"`+columns.join(`","`)+`"`+"\r\n";
+          // ## csv
+          let csv = `"` + columns.join(`","`) + `"` + '\r\n'
 
-                    // ## json-schema -> json-schema-table
-                    const table = {
-                        fields:[]
-                    };
-                    for(let i = 0, l = columns.length; i<l; i++) {
-                        const key = columns[i];
-                        const field = schemaJSON.properties[key];
-                        const column = {
-                            name:key,
-                            title:field.title,
-                            description:field.description,
-                            type:field.type,
-                            constraints:{
-                                required: (schemaJSON.required.indexOf(key) !== -1) ? true : null
-                            }
-                        };
-                        if (field.hasOwnProperty('format')) {
-                            column.format = field.format;
-                            if (field.format === 'date-time') {
-                                column.format = field.format.replace('-', '');
-                            }
-                        }
+          // ## json-schema -> json-schema-table
+          const table = {
+            fields: []
+          }
+          for (let i = 0, l = columns.length; i < l; i++) {
+            const key = columns[i]
+            const field = schemaJSON.properties[key]
+            const column = {
+              name: key,
+              title: field.title,
+              description: field.description,
+              type: field.type,
+              constraints: {
+                required: (schemaJSON.required.indexOf(key) !== -1) ? true : null
+              }
+            }
+            if (field.hasOwnProperty('format')) {
+              column.format = field.format
+              if (field.format === 'date-time') {
+                column.format = field.format.replace('-', '')
+              }
+            }
 
-                        const constraints = ['minLength','maxLength','minimum','maximum','pattern','enum'];
-                        for(let j = 0, m = constraints.length; j<m; j++) {
-                            if (field.hasOwnProperty(constraints[j])) {
-                                column.constraints[constraints[j]] = field[constraints[j]];
-                            }
-                        }
+            const constraints = ['minLength', 'maxLength', 'minimum', 'maximum', 'pattern', 'enum']
+            for (let j = 0, m = constraints.length; j < m; j++) {
+              if (field.hasOwnProperty(constraints[j])) {
+                column.constraints[constraints[j]] = field[constraints[j]]
+              }
+            }
 
-                        table.fields.push(column);
-                    }
+            table.fields.push(column)
+          }
 
-                    // ## sql
-                    let sql = `
-CREATE SCHEMA IF NOT EXISTS datasets;
-CREATE TABLE IF NOT EXISTS datasets.data (
-  tenant                           VARCHAR(60) NOT NULL,
-  id                               VARCHAR(60) NOT NULL,
-  create_timestamp                 TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
-`;
-                    for(let i = 0, l = columns.length; i<l; i++) {
-                        const key = columns[i];
-                        if (key.indexOf('Time') !== -1) continue;   // covered by previous column (DateTime)
-                        sql += `  ${changeCase.snake(key.replace('Date', 'Timestamp'))}`;
+          // ## sql
+          let sql = `
+CREATE SCHEMA IF NOT EXISTS datastream;
+CREATE TABLE IF NOT EXISTS datastream.samples (
+  project_id     UUID NOT NULL,
+`
+          for (let i = 0, l = columns.length; i < l; i++) {
+            const key = columns[i]
+            if (key.indexOf('Time') !== -1) continue   // covered by previous column (DateTime)
+            sql += `  ${changeCase.snake(key.replace('Date', 'Timestamp'))}`
 
-                        const field = schemaJSON.properties[key];
+            const field = schemaJSON.properties[key]
 
-                        if (field.type === 'string' && field.maxLength) {
-                            sql += ` VARCHAR(${field.maxLength})`;
-                        } else if (field.type === 'string' && field.format === 'date') {
-                            sql += ` TIMESTAMP WITH TIME ZONE`;
-                        }  else if (field.type === 'string') {
-                            sql += ` TEXT`;
-                        } else if (field.type === 'number') {
-                            sql += ` NUMERIC`;
-                        }
+            if (field.type === 'string' && field.maxLength) {
+              sql += ` VARCHAR(${field.maxLength})`
+            } else if (field.type === 'string' && field.format === 'date') {
+              sql += ` TIMESTAMP WITH TIME ZONE`
+            } else if (field.type === 'string') {
+              sql += ` TEXT`
+            } else if (field.type === 'number') {
+              sql += ` NUMERIC`
+            }
 
-                        if (field.default) {
-                            sql += ` DEFAULT '${field.default}'`;
-                        } else if (schemaJSON.required.indexOf(key) !== -1) {
-                            sql += ` NOT NULL`;
-                        }
+            if (field.default) {
+              sql += ` DEFAULT '${field.default}'`
+            } else if (schemaJSON.required.indexOf(key) !== -1) {
+              sql += ` NOT NULL`
+            }
 
-                        if (i !== columns.length -1) {
-                            sql += `,
-`;
-                        }
-                    }
-                    sql += `
+            if (i !== columns.length -1) {
+              sql += `,
+`
+            }
+          }
+          sql += `
 );
 
-CREATE UNIQUE INDEX IF NOT EXISTS pkey ON datasets.data (
-  id,
+CREATE UNIQUE INDEX IF NOT EXISTS sample_pkey ON datastream.samples (
+  project_id,
   monitoring_location_latitude,
   monitoring_location_longitude,
   activity_start_timestamp,
@@ -138,54 +138,42 @@ CREATE UNIQUE INDEX IF NOT EXISTS pkey ON datasets.data (
   result_sample_fraction,
   result_analytical_method_id
 );
-CREATE INDEX IF NOT EXISTS tenant_idx ON datasets.data (tenant);
-CREATE INDEX IF NOT EXISTS upload_id_idx ON datasets.data (upload_id);
-CREATE INDEX IF NOT EXISTS latitude_idx ON datasets.data (monitoring_location_latitude);
-CREATE INDEX IF NOT EXISTS longitude_idx ON datasets.data (monitoring_location_longitude);
+CREATE INDEX IF NOT EXISTS sample_project_id_idx ON datastream.samples (project_id);
+CREATE INDEX IF NOT EXISTS sample_latitude_idx ON datastream.samples (monitoring_location_latitude);
+CREATE INDEX IF NOT EXISTS sample_longitude_idx ON datastream.samples (monitoring_location_longitude);
+`
 
-CREATE TABLE IF NOT EXISTS datasets.meta (
-  tenant        VARCHAR(60) NOT NULL,
-  id            VARCHAR(60) UNIQUE NOT NULL,
-  program_id    INTEGER,
-  user_id       VARCHAR(64) NOT NULL,
-  title         VARCHAR(255),
-  description   TEXT,
-  created       TIMESTAMP DEFAULT NOW(),
-  modified      TIMESTAMP DEFAULT NOW()
-);
-`;
+          // compiled ajv
+          const validate = ajv.compile(schemaJSON)
+          const moduleCode = pack(ajv, validate)
 
-                    // compiled ajv
-                    const validate = ajv.compile(schemaJSON);
-                    const moduleCode = pack(ajv, validate);
-
-                    return Promise.all([
-                        writeFile(validateFile, repalce(moduleCode), {encoding:'utf8'}),
-                        writeFile(csvFile, csv, {encoding:'utf8'}),
-                        writeFile(jsonSchemaFile, repalce(JSON.stringify(schemaJSON, null, 2)), {encoding:'utf8'}),
-                        writeFile(jsonTableSchemaFile, repalce(JSON.stringify(table, null, 2)), {encoding:'utf8'}),
-                        writeFile(sqlFile, sql, {encoding:'utf8'})
-                    ]);
-                })
-                .catch((err) => {
-                    console.error('Error: deref', filePath, err);
-                });
-            arr.push(deref);
-        });
-        return Promise.all(arr);
+          return Promise.all([
+            writeFile(validateFile, replace(moduleCode), {encoding: 'utf8'}),
+            writeFile(csvFile, csv, {encoding: 'utf8'}),
+            writeFile(jsonSchemaFile, replace(JSON.stringify(schemaJSON, null, 2)), {encoding: 'utf8'}),
+            writeFile(jsonTableSchemaFile, replace(JSON.stringify(table, null, 2)), {encoding: 'utf8'}),
+            writeFile(sqlFile, sql, {encoding: 'utf8'})
+          ])
+        })
+        .catch((err) => {
+          console.error('Error: deref', filePath, err)
+        })
+      arr.push(deref)
     })
-    .catch((err) => {
-        console.error('Error: glob', err);
-    })
-    .then(() => {
-        console.log('Compile: Complete');
-        console.log('Copy package.json');
-        const npm = require(__dirname+'/../package.json');
+    return Promise.all(arr)
+  })
+  .catch((err) => {
+    console.error('Error: glob', err)
+  })
+  .then(() => {
+    console.log('Compile: Complete')
+    console.log('Copy package.json')
+    const npm = require(__dirname + '/../package.json')
 
-        delete npm.scripts;
-        delete npm.devDependencies;
+    delete npm.scripts
+    delete npm.devDependencies
 
-        fs.writeFileSync(__dirname+'/../dist/package.json', JSON.stringify(npm, null, 2), {encoding:'utf8'});
+    fs.writeFileSync(__dirname + '/../dist/package.json', JSON.stringify(npm, null, 2), {encoding: 'utf8'})
 
-        console.log('Done!');
-    });
+    console.log('Done!')
+  })
--- test/index.js
+++ test/index.js
@@ -1,136 +1,176 @@
-const expect = require('chai').expect;
+const expect = require('chai').expect
 
 //const istanbul = require('istanbul');
-const Ajv = require('ajv');
+const Ajv = require('ajv')
 //const ajvIstanbul = require('ajv-istanbul');
-const schema = require('../dist/json-schema');
+const schema = require('../dist/json-schema')
 
 const ajv = new Ajv({
-    v5: true,
-    format:'full',
-    coerceTypes: true,
-    allErrors: true,
-    useDefaults: true
-});
+  v5: true,
+  format: 'full',
+  coerceTypes: true,
+  allErrors: true,
+  useDefaults: true
+})
+require('ajv-keywords')(ajv, ['switch','transform'])
 //ajvIstanbul(ajv);
-const validate = ajv.compile(schema);
+const validate = ajv.compile(schema)
 
 const checkMissingProperty = (errors, keyword, property) => {
-    for(let i = errors.length; i--; i) {
-        const error = validate.errors[i];
-        if (error.keyword !== keyword) continue;
-        if (error.params.missingProperty === property) return true;
-    }
-    return false;
-};
+  for (let i = errors.length; i--; i) {
+    const error = validate.errors[i]
+    if (error.keyword !== keyword) continue
+    if (error.params.missingProperty === property) return true
+  }
+  return false
+}
 
 describe('DataStream Schema', function () {
 
-    it('Should set defaults', function (done) {
-        let data = {};
-        const valid = validate(data);
-        expect(valid).to.equal(false);
+  it('Should set defaults', function (done) {
+    let data = {}
+    const valid = validate(data)
+    expect(valid).to.equal(false)
 
-        expect(data.MonitoringLocationHorizontalCoordinateReferenceSystem).to.equal('UNKWN');
-        expect(data.ActivityMediaName).to.equal('Surface Water');
-        expect(Object.keys(data).length).to.equal(2);
+    expect(data.MonitoringLocationHorizontalCoordinateReferenceSystem).to.equal('UNKWN')
+    expect(data.ActivityMediaName).to.equal('Water')
+    expect(Object.keys(data).length).to.equal(2)
 
-        done();
+    done()
+  })
+
+  it('Should require properties', function (done) {
+    const valid = validate({})
+    expect(valid).to.equal(false)
+
+    // check required error out
+    schema.required.forEach((property) => {
+      if (property === 'MonitoringLocationHorizontalCoordinateReferenceSystem') return
+      if (property === 'ActivityMediaName') return
+      expect(checkMissingProperty(validate.errors, 'required', property)).to.equal(true)
+    })
+
+    done()
+  })
+
+  describe('Should enforce conditional required', function () {
+    it('ResultValue & ResultDetectionCondition', function (done) {
+      const valid = validate({})
+      expect(valid).to.equal(false)
+      expect(checkMissingProperty(validate.errors, 'required', 'ResultValue')).to.equal(true)
+      expect(checkMissingProperty(validate.errors, 'required', 'ResultDetectionCondition')).to.equal(true)
+      done()
+    })
+
+    it('ResultValue', function (done) {
+      const valid = validate({
+        'ResultValue': true,
+        'ResultAnalyticalMethodID': true
+      })
+      expect(valid).to.equal(false)
+      expect(checkMissingProperty(validate.errors, 'required', 'ResultDetectionCondition')).to.equal(false)
+      done()
+    })
+
+    it('ResultDetectionCondition', function (done) {
+      const valid = validate({
+        'ResultDetectionCondition': true,
+        'ResultAnalyticalMethodID': true
+      })
+      expect(valid).to.equal(false)
+      expect(checkMissingProperty(validate.errors, 'required', 'ResultValue')).to.equal(false)
+      done()
+    })
+
+    it('ResultAnalyticalMethodID & ResultAnalyticalMethodName', function (done) {
+      const valid = validate({})
+      expect(valid).to.equal(false)
+      expect(checkMissingProperty(validate.errors, 'required', 'ResultAnalyticalMethodID')).to.equal(true)
+      expect(checkMissingProperty(validate.errors, 'required', 'ResultAnalyticalMethodName')).to.equal(true)
+      done()
+    })
+
+    it('ResultAnalyticalMethodID', function (done) {
+      const valid = validate({
+        'ResultValue': true,
+        'ResultAnalyticalMethodID': true
+      })
+      expect(valid).to.equal(false)
+      expect(checkMissingProperty(validate.errors, 'required', 'ResultAnalyticalMethodName')).to.equal(false)
+      done()
+    })
+
+    it('ResultAnalyticalMethodName', function (done) {
+      const valid = validate({
+        'ResultValue': true,
+        'ResultAnalyticalMethodName': true
+      })
+      expect(valid).to.equal(false)
+      expect(checkMissingProperty(validate.errors, 'required', 'ResultAnalyticalMethodIDResultAnalyticalMethodID')).to.equal(false)
+      done()
+    })
+  })
+
+  describe('Should require based on switch', function() {
+    it('ResultDetectionCondition true', function (done) {
+      const valid = validate({
+        'ResultDetectionCondition':'Present Above Quantification Limit'
+      });
+      expect(valid).to.equal(false);
+      expect(checkMissingProperty(validate.errors, 'required', 'ResultDetectionQuantitationLimitMeasure')).to.equal(true);
+      done();
     });
 
-    it('Should require properties', function (done) {
-        const valid = validate({});
-        expect(valid).to.equal(false);
-
-        // check required error out
-        schema.required.forEach((property) => {
-            if (property === 'MonitoringLocationHorizontalCoordinateReferenceSystem') return;
-            if (property === 'ActivityMediaName') return;
-            expect(checkMissingProperty(validate.errors, 'required', property)).to.equal(true);
-        });
-
-        done();
+    it('ResultDetectionCondition false', function (done) {
+      const valid = validate({
+        'ResultDetectionCondition':false
+      });
+      expect(valid).to.equal(false);
+      expect(checkMissingProperty(validate.errors, 'required', 'ResultDetectionQuantitationLimitMeasure')).to.equal(false);
+      done();
     });
+  })
 
-    describe('Should enforce conditional required', function() {
-        it('ResultValue & ResultDetectionCondition', function (done) {
-            const valid = validate({});
-            expect(valid).to.equal(false);
-            expect(checkMissingProperty(validate.errors, 'required', 'ResultValue')).to.equal(true);
-            expect(checkMissingProperty(validate.errors, 'required', 'ResultDetectionCondition')).to.equal(true);
-            done();
-        });
+  describe('Should require dependencies', function () {
+    it('ActivityDepthHeightMeasure', function (done) {
+      const valid = validate({
+        'ActivityDepthHeightMeasure': true
+      })
+      expect(valid).to.equal(false)
+      expect(checkMissingProperty(validate.errors, 'dependencies', 'ActivityDepthHeightUnit')).to.equal(true)
+      done()
+    })
 
-        it('ResultValue', function (done) {
-            const valid = validate({
-                "ResultValue":true
-            });
-            expect(valid).to.equal(false);
-            expect(checkMissingProperty(validate.errors, 'required', 'ResultDetectionCondition')).to.equal(false);
-            done();
-        });
+    it('ResultValue', function (done) {
+      const valid = validate({
+        'ResultValue': true
+      })
+      expect(valid).to.equal(false)
+      expect(checkMissingProperty(validate.errors, 'dependencies', 'ResultUnit')).to.equal(true)
+      done()
+    })
 
-        it('ResultDetectionCondition', function (done) {
-            const valid = validate({
-                "ResultDetectionCondition":true
-            });
-            expect(valid).to.equal(false);
-            expect(checkMissingProperty(validate.errors, 'required', 'ResultValue')).to.equal(false);
-            done();
-        });
-    });
+    it('ResultDetectionQuantitationLimitMeasure', function (done) {
+      const valid = validate({
+        'ResultDetectionQuantitationLimitMeasure': true
+      })
+      expect(valid).to.equal(false)
+      expect(checkMissingProperty(validate.errors, 'dependencies', 'ResultDetectionQuantitationLimitUnit')).to.equal(true)
+      expect(checkMissingProperty(validate.errors, 'dependencies', 'ResultDetectionQuantitationLimitType')).to.equal(true)
+      done()
+    })
 
-    describe('Should require dependencies', function() {
-        it('ActivityDepthHeightMeasure', function (done) {
-            const valid = validate({
-                "ActivityDepthHeightMeasure":true
-            });
-            expect(valid).to.equal(false);
-            expect(checkMissingProperty(validate.errors, 'dependencies', 'ActivityDepthHeightUnit')).to.equal(true);
-            done();
-        });
+    it('ResultAnalyticalMethodID', function (done) {
+      const valid = validate({
+        'ResultAnalyticalMethodID': true
+      })
+      expect(valid).to.equal(false)
+      expect(checkMissingProperty(validate.errors, 'dependencies', 'ResultAnalyticalMethodContext')).to.equal(true)
+      done()
+    })
+  })
 
-        it('ResultValue', function (done) {
-            const valid = validate({
-                "ResultValue":true
-            });
-            expect(valid).to.equal(false);
-            expect(checkMissingProperty(validate.errors, 'dependencies', 'ResultUnit')).to.equal(true);
-            done();
-        });
-
-        it('ResultDetectionCondition', function (done) {
-            const valid = validate({
-                "ResultDetectionCondition":true
-            });
-            expect(valid).to.equal(false);
-            expect(checkMissingProperty(validate.errors, 'dependencies', 'ResultDetectionQuantitationLimitMeasure')).to.equal(true);
-            done();
-        });
-
-        it('ResultDetectionQuantitationLimitMeasure', function (done) {
-            const valid = validate({
-                "ResultDetectionQuantitationLimitMeasure":true
-            });
-            expect(valid).to.equal(false);
-            expect(checkMissingProperty(validate.errors, 'dependencies', 'ResultDetectionQuantitationLimitUnit')).to.equal(true);
-            expect(checkMissingProperty(validate.errors, 'dependencies', 'ResultDetectionQuantitationLimitType')).to.equal(true);
-            done();
-        });
-
-        it('ResultAnalyticalMethodID', function (done) {
-            const valid = validate({
-                "ResultAnalyticalMethodID":true
-            });
-            expect(valid).to.equal(false);
-            expect(checkMissingProperty(validate.errors, 'dependencies', 'ResultAnalyticalMethodContext')).to.equal(true);
-            expect(checkMissingProperty(validate.errors, 'dependencies', 'ResultAnalyticalMethodName')).to.equal(true);
-            done();
-        });
-    });
-
-});
-
+})
 
 // Print out coverage report
 // const collector = new istanbul.Collector();
