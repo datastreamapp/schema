2250bb0f3a97a664249dac98adb624cc804b81d9
will Farrell <will.farrell@gmail.com>
2018-05-08 15:41:48 -0600

    fix: update csv source for CharacteristicName

--- bin/build-switch-if-characteristic-name.sh
+++ bin/build-switch-if-characteristic-name.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-LIST=$(grep "Yes" lib/CharacteristicName.csv | grep -E ',\r?\n?$' | sed 's/,Yes.*//' | sed 's/"//g')
+LIST=$(grep "Yes" lib/CharacteristicName.csv | sed 's/,Yes.*//' | sed 's/"//g')
 JSON=$(jq -ncR '[inputs]' <<< "$LIST")
 
 cat << EOF > src/definitions.switch-if-characteristic-name.json
