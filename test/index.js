671252fc502a16853d4d356d1635b21d48a0ab1d
will Farrell <will.farrell@gmail.com>
2020-03-03 09:40:26 -0700

    fix: nested logic bug
    - `ResultAnalyticalMethodContext ` add `EN`
    - remove or condition on `ResultAnalyticalMethodID` and `ResultAnalyticalMethodName`

--- test/index.js
+++ test/index.js
@@ -4,24 +4,24 @@ const schema = require('../dist/json-schema')
 //const validate = require('../dist/validate')
 
 // Start Manual
-const Ajv = require('ajv');
+const Ajv = require('ajv')
 
 const ajv = new Ajv({
   v5: true,
-  format:'full',
+  format: 'full',
   coerceTypes: true,
   allErrors: true,
   useDefaults: true
-});
+})
 require('ajv-keywords')(ajv, ['transform'])
-const validate = ajv.compile(schema);
+const validate = ajv.compile(schema)
 // End Manual
 
 const checkMissingProperty = (errors, keyword, property) => {
   for (let i = errors.length; i--; i) {
     const error = validate.errors[i]
     if (error.keyword !== keyword) continue
-    if (['required','dependencies'].includes(keyword) && error.params.missingProperty === property) return true
+    if (['required', 'dependencies'].includes(keyword) && error.params.missingProperty === property) return true
     else if (keyword === 'additionalProperties' && error.params.additionalProperty === property) return true
   }
   return false
@@ -56,37 +56,37 @@ describe('DataStream Schema', function () {
 
   it('Should not require properties', function (done) {
     const valid = validate({
-      "DatasetName":"Test",
-      "MonitoringLocationID":"A1",
-      "MonitoringLocationName":"A1 Test",
-      "MonitoringLocationLatitude":"51.0486",
-      "MonitoringLocationLongitude":"-114.0708",
-      "MonitoringLocationHorizontalCoordinateReferenceSystem":"AMSMA",
-      "MonitoringLocationType":"Atmosphere",
-      "MonitoringLocationWaterbody":"Elbow River",
-      "ActivityType":"Field Msr/Obs",
-      "ActivityMediaName":"Water",
-      "ActivityDepthHeightMeasure":"-34",
-      "ActivityDepthHeightUnit":"m",
-      "SampleCollectionEquipmentName":"Bongo Net",
-      "CharacteristicName":"Aluminum",
-      "MethodSpeciation":"as B",
-      "ResultSampleFraction":"Dissolved",
-      "ResultValue":"99.99",
-      "ResultUnit":"µg",
-      "ResultStatusID":"Accepted",
-      "ResultComment":"None at this time",
-      "ResultAnalyticalMethodID":"1",
-      "ResultAnalyticalMethodContext":"APHA",
-      "ActivityStartDate":"2018-02-23",
-      "ActivityStartTime":"13:15:00",
-      "ActivityEndDate":"2018-02-23",
-      "ActivityEndTime":"13:15:00",
-      "LaboratoryName":"Farrell Labs",
-      "LaboratorySampleID":"101010011110",
-      "AnalysisStartDate":"2018-02-23",
-      "AnalysisStartTime":"13:15:00",
-      "AnalysisStartTimeZone":"-0600"
+      'DatasetName': 'Test',
+      'MonitoringLocationID': 'A1',
+      'MonitoringLocationName': 'A1 Test',
+      'MonitoringLocationLatitude': '51.0486',
+      'MonitoringLocationLongitude': '-114.0708',
+      'MonitoringLocationHorizontalCoordinateReferenceSystem': 'AMSMA',
+      'MonitoringLocationType': 'Atmosphere',
+      'MonitoringLocationWaterbody': 'Elbow River',
+      'ActivityType': 'Field Msr/Obs',
+      'ActivityMediaName': 'Water',
+      'ActivityDepthHeightMeasure': '-34',
+      'ActivityDepthHeightUnit': 'm',
+      'SampleCollectionEquipmentName': 'Bongo Net',
+      'CharacteristicName': 'Aluminum',
+      'MethodSpeciation': 'as B',
+      'ResultSampleFraction': 'Dissolved',
+      'ResultValue': '99.99',
+      'ResultUnit': 'µg',
+      'ResultStatusID': 'Accepted',
+      'ResultComment': 'None at this time',
+      'ResultAnalyticalMethodID': '1',
+      'ResultAnalyticalMethodContext': 'APHA',
+      'ActivityStartDate': '2018-02-23',
+      'ActivityStartTime': '13:15:00',
+      'ActivityEndDate': '2018-02-23',
+      'ActivityEndTime': '13:15:00',
+      'LaboratoryName': 'Farrell Labs',
+      'LaboratorySampleID': '101010011110',
+      'AnalysisStartDate': '2018-02-23',
+      'AnalysisStartTime': '13:15:00',
+      'AnalysisStartTimeZone': '-0600'
     })
     //console.log(validate.errors)
     expect(valid).to.equal(true)
@@ -95,11 +95,10 @@ describe('DataStream Schema', function () {
 
   it('Should reject additional headers', function (done) {
     const valid = validate({
-      "MonitoringLocationWaterBody":"Lake"
+      'MonitoringLocationWaterBody': 'Lake'
     })
     expect(valid).to.equal(false)
     expect(checkMissingProperty(validate.errors, 'additionalProperties', 'MonitoringLocationWaterBody')).to.equal(true)
-
 
     done()
   })
@@ -170,42 +169,42 @@ describe('DataStream Schema', function () {
     })
   })
 
-  describe('Should require based on nested if (switch)', function() {
+  describe('Should require based on nested if (switch)', function () {
     it('ResultDetectionCondition true', function (done) {
       const valid = validate({
-        'ResultDetectionCondition':'Present Above Quantification Limit'
-      });
-      expect(valid).to.equal(false);
-      expect(checkMissingProperty(validate.errors, 'dependencies', 'ResultDetectionQuantitationLimitMeasure')).to.equal(true);
-      done();
-    });
+        'ResultDetectionCondition': 'Present Above Quantification Limit'
+      })
+      expect(valid).to.equal(false)
+      expect(checkMissingProperty(validate.errors, 'dependencies', 'ResultDetectionQuantitationLimitMeasure')).to.equal(true)
+      done()
+    })
 
     it('ResultDetectionCondition true', function (done) {
       const valid = validate({
-        'ResultDetectionCondition':'Present Below Quantification Limit'
-      });
-      expect(valid).to.equal(false);
-      expect(checkMissingProperty(validate.errors, 'dependencies', 'ResultDetectionQuantitationLimitMeasure')).to.equal(true);
-      done();
-    });
+        'ResultDetectionCondition': 'Present Below Quantification Limit'
+      })
+      expect(valid).to.equal(false)
+      expect(checkMissingProperty(validate.errors, 'dependencies', 'ResultDetectionQuantitationLimitMeasure')).to.equal(true)
+      done()
+    })
 
     it('ResultDetectionCondition false', function (done) {
       const valid = validate({
-        'ResultDetectionCondition':''
-      });
-      expect(valid).to.equal(false);
-      expect(checkMissingProperty(validate.errors, 'dependencies', 'ResultDetectionQuantitationLimitMeasure')).to.equal(false);
-      done();
-    });
+        'ResultDetectionCondition': ''
+      })
+      expect(valid).to.equal(false)
+      expect(checkMissingProperty(validate.errors, 'dependencies', 'ResultDetectionQuantitationLimitMeasure')).to.equal(false)
+      done()
+    })
 
     it('CharacteristicName true', function (done) {
       const valid = validate({
-        'CharacteristicName':'Silver'
-      });
-      expect(valid).to.equal(false);
-      expect(checkMissingProperty(validate.errors, 'required', 'ResultSampleFraction')).to.equal(true);
-      done();
-    });
+        'CharacteristicName': 'Silver'
+      })
+      expect(valid).to.equal(false)
+      expect(checkMissingProperty(validate.errors, 'required', 'ResultSampleFraction')).to.equal(true)
+      done()
+    })
 
   })
 
@@ -248,26 +247,60 @@ describe('DataStream Schema', function () {
     })
   })
 
-  it('Should validate time', function(done) {
+  it('Should validate time', function (done) {
     const valid = validate({
 
-      "ActivityStartTime":"9:15:00",
-      "ActivityEndTime":"13:15:00.000",
-      "AnalysisStartTime":"2:15"
+      'ActivityStartTime': '9:15:00',
+      'ActivityEndTime': '13:15:00.000',
+      'AnalysisStartTime': '2:15'
     })
-    console.log(validate.errors.filter((i) => i.keyword === 'pattern'))
     expect(validate.errors.filter((i) => i.keyword === 'pattern').length).to.equal(0)
     done()
   })
 
-  it('Should validate wrong time', function(done) {
+  it('Should validate wrong time', function (done) {
     const valid = validate({
-      "ActivityStartTime":"9:15.00"
+      'ActivityStartTime': '9:15.00'
     })
-    console.log(validate.errors.filter((i) => i.keyword === 'pattern'))
     expect(validate.errors.filter((i) => i.keyword === 'pattern').length).to.equal(1)
     done()
   })
+
+  /*it('Should fail manual test', function (done) {
+    const valid = validate(
+      {
+        'DatasetName': 'Lac La Biche County Lake Water Quality Monitoring Program',
+        'MonitoringLocationName': 'Lac La Biche East',
+        'MonitoringLocationLatitude': 54.83068572,
+        'MonitoringLocationLongitude': -111.9159406,
+        'MonitoringLocationHorizontalCoordinateReferenceSystem': 'WGS84',
+        'MonitoringLocationType': 'Lake/Pond',
+        'MonitoringLocationWaterbody': 'Lac La Biche',
+        'ActivityType': 'Sample-Composite Without Parents',
+        'ActivityMediaName': 'Surface Water',
+        'ActivityStartDate': '2011-07-25',
+        'ActivityStartTime': '13:30:00',
+        'SampleCollectionEquipmentName': 'Kemmerer Bottle',
+        'CharacteristicName': 'Hydroxide',
+        'ResultSampleFraction': 'Total',
+        'ResultValueType': 'Actual',
+        'ResultDetectionCondition': 'Below Detection/Quantification Limit',
+        'ResultDetectionQuantitationLimitMeasure': 5,
+        'ResultDetectionQuantitationLimitUnit': 'mg/l',
+        'ResultDetectionQuantitationLimitType': 'Method Detection Level',
+        'ResultComment': 'A composite sample taken from 3 locations around the lake.',
+        'ResultAnalyticalMethodID': '4500-H, 2510, 2320',
+        'ResultAnalyticalMethodContext': 'APHA',
+        'ResultAnalyticalMethodName': 'pH, Conductivity and Total Alkalinity',
+        'AnalysisStartDate': '2011-07-29',
+        'LaboratoryName': 'ALS',
+        'LaboratorySampleID': 'L1037374-1'
+      }
+    )
+    console.log(JSON.stringify(validate.errors, null, 2))
+    expect(0).to.equal(1)
+    done()
+  })*/
 
 })
 
