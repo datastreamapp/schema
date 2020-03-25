fe70522ff74b7d2fc75087d3bf8de91172dffdb9
will Farrell <will.farrell@gmail.com>
2020-03-25 15:23:47 -0600

    fix: clean up file naming

--- bin/build-logic-characteristic-name.sh
+++ bin/build-logic-characteristic-name.sh
@@ -9,7 +9,7 @@ LIST=$(grep "Yes" lib/CharacteristicName.csv | sed 's/,Yes.*//' | sed 's/"//g')
 JSON=$(jq -ncR '[inputs]' <<< "$LIST")
 
 # If one of the following characteristic name, then sample fraction is required
-cat << EOF > src/definitions.switch-if-characteristic-name.json
+cat << EOF > src/definitions.logic.characteristic-name.json
 {
   "properties": {
     "CharacteristicName": {
