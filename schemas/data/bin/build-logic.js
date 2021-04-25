047027a9a73b760b10b8256337ef56bd9573ff07
will Farrell <will.farrell@gmail.com>
2021-04-24 22:52:02 -0600

    feat: add in new qc checks

--- schemas/data/bin/build-logic.js
+++ schemas/data/bin/build-logic.js
@@ -1,8 +1,7 @@
-
 const fs = require('fs')
-const {subtractions} = require('./build-values')
+const { subtractions } = require('./build-values')
 
-const wqxRequiredIf = async(file) => {
+const wqxRequiredIf = async (file) => {
   console.log(file)
   const object = require(`wqx/required/${file}.json`)
 
@@ -10,7 +9,33 @@ const wqxRequiredIf = async(file) => {
   const list = [...new Set(object.if.properties[column].enum.sort())]
   object.if.properties[column].enum = subtractions(column, list)
 
-  fs.writeFileSync(__dirname + `/../src/logic/${file}.json`, JSON.stringify(object, null, 2), {encoding: 'utf8'})
+  fs.writeFileSync(__dirname + `/../src/logic/${file}.json`, JSON.stringify(object, null, 2), { encoding: 'utf8' })
+
+  const qc = {
+    'if': {
+      'properties': {
+        'CharacteristicName': {
+          'not': {
+            'enum': list
+          }
+        }
+      },
+      'required': [
+        'CharacteristicName'
+      ]
+    },
+    'then': {
+      'properties': {
+        [file.split('-')[1]]: {
+          'enum': [null, '']
+        }
+      },
+      'required': [
+        'MethodSpeciation'
+      ]
+    }
+  }
+  fs.writeFileSync(__dirname + `/../src/quality-control/${file}.json`, JSON.stringify(qc, null, 2), { encoding: 'utf8' })
 }
 
 //wqxRequiredIf('ActivityType-AnalyticalMethod','CharacteristicName',['AnalyticalMethodtype'])
