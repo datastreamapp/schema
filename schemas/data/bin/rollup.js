17d760714942ae25fa636b004654fc69cb84b0b9
will Farrell <will.farrell@gmail.com>
2021-06-10 13:26:01 -0600

    fix: nutrient logic

--- schemas/data/bin/rollup.js
+++ schemas/data/bin/rollup.js
@@ -1,40 +1,29 @@
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
-import {terser} from 'rollup-plugin-terser' // TODO esbuild
+import { terser } from 'rollup-plugin-terser' // TODO esbuild
 
 function onwarn (warning, warn) {
-  if (warning.code === 'CIRCULAR_DEPENDENCY') return;
-  warn(warning);
+  if (warning.code === 'CIRCULAR_DEPENDENCY') return
+  warn(warning)
 }
 
-export default [{
-  input: 'primary/index.js',
-  name: 'primary'
-},{
-  input: 'frontend/index.js',
-  name: 'frontend'
-},{
-  input: 'backend/index.js',
-  name: 'backend'
-},{
-  input: 'quality-control/index.js',
-  name: 'quality-control'
-}].map(bundle => ({
-  input: bundle.input,
-  output: [{
-    // When included in a browser facing app and using rollup, can't use sub folders
-    dir: bundle.input.replace('/index.js', ''),
-    entryFileNames: '[name].mjs',
-    format: 'es'
-  },{
-    dir: bundle.input.replace('/index.js', ''),
-    entryFileNames: '[name].js',
-    format: 'cjs'
-  }],
-  plugins: [
-    resolve(),
-    commonjs(),
-    terser(),
-  ],
-  onwarn,
-}));
+export default ['primary', 'frontend', 'backend', 'quality-control']
+  .map(name => ({
+    input: `${name}/index.js`,
+    output: [{
+      // When included in a browser facing app and using rollup, can't use sub folders
+      dir: name,
+      entryFileNames: '[name].mjs',
+      format: 'es'
+    }, {
+      dir: name,
+      entryFileNames: '[name].js',
+      format: 'cjs'
+    }],
+    plugins: [
+      resolve(),
+      commonjs(),
+      terser(),
+    ],
+    onwarn,
+  }))
