05781326aaaf4cb0c1eea4319a479ef2c87c52cc
will Farrell <will.farrell@gmail.com>
2022-06-02 13:34:25 -0600

    feat: add in lookups

--- schemas/data/bin/build-init.sh
+++ schemas/data/bin/build-init.sh
@@ -6,3 +6,5 @@ mkdir -p primary
 mkdir -p frontend
 mkdir -p backend
 mkdir -p quality-control
+mkdir -p lookup
+cp src/lookup/* lookup/
--- schemas/data/bin/build-values.js
+++ schemas/data/bin/build-values.js
@@ -57,33 +57,3 @@ for (const col of Object.keys(wqx)) {
     { encoding: "utf8" }
   );
 }
-
-// Check CharacteristicNameGroups
-let characteristicNameGroups = await readFile(
-  join(__dirname, `../node_modules/wqx/groups/CharacteristicName.json`)
-).then((res) => JSON.parse(res))
-let characteristicNameGroupsLocal = await readFile(
-  join(__dirname, `../src/groups/CharacteristicName.json`)
-).then((res) => JSON.parse(res))
-
-for(const key in characteristicNameGroupsLocal) {
-  if (characteristicNameGroups[key]) {
-    console.log(`CharacteristicNameGroup "${key}":"${characteristicNameGroups[key]}" replace with "${characteristicNameGroupsLocal[key]}"`)
-  }
-  characteristicNameGroups[key] = characteristicNameGroupsLocal[key]
-}
-
-let characteristicNames = await readFile(
-  join(__dirname, `../src/values/CharacteristicName.primary.json`)
-).then((res) => JSON.parse(res))
-characteristicNames = characteristicNames.enum;
-
-for (const characteristicName of characteristicNames) {
-  //console.log(characteristicName, characteristicNameGroups[characteristicName])
-  if (
-    !characteristicNameGroups[characteristicName] ||
-    characteristicNameGroups[characteristicName] === "Not Assigned"
-  ) {
-    console.log("CharacteristicNameGroups Not Assigned", characteristicName);
-  }
-}
