0d9b0ab6363e83a2caa93ea6d42f2a3a15164630
will Farrell <will.farrell@gmail.com>
2022-06-20 11:57:34 -0600

    ci: add template build script

---
+++ schemas/data/bin/build-macos.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+mkdir ./bin/template
+
+npm install
+
+mkdir -p primary
+mkdir -p frontend
+mkdir -p backend
+mkdir -p quality-control
+
+node ./bin/build-values.js
+node ./bin/build-logic.js
+
+node ./bin/build.js
+node ./bin/build-template.js
+
+ava
