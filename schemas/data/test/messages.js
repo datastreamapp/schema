689f08140dd926d41aeb12a0eb8986b0630dd351
will Farrell <willfarrell@proton.me>
2023-05-02 12:37:00 -0600

    fix: add missing char that need sf

---
+++ schemas/data/test/messages.js
@@ -0,0 +1,15 @@
+import test from 'ava'
+
+import en from '../locales/en-CA.js'
+import validate from '../quality-control/index.js'
+
+test('Should transform error message', async (t) => {
+  const valid = validate({
+    ActivityType: 'Field Msr/Obs',
+    ResultSampleFraction: 'Filtered'
+  })
+  console.log(validate.errors)
+  en(validate.errors)
+  console.log(validate.errors)
+  t.is(valid, false)
+})
