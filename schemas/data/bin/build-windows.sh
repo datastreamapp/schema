3cc782e206ca8d3825cc08874555bdb1f8810655
Jayakishore Roy <jayakishore@datastream.org>
2024-02-14 16:54:55 -0500

    chore: update windows build script

--- schemas/data/bin/build-windows.sh
+++ schemas/data/bin/build-windows.sh
@@ -5,14 +5,20 @@ mkdir .\\bin\\template
 npm install
 
 mkdir -p primary
+mkdir -p extract
 mkdir -p frontend
 mkdir -p backend
 mkdir -p quality-control
+mkdir -p lookup
+mkdir -p locales
+cp src/lookup/* lookup/
 
 node .\\bin\\build-values.js
 node .\\bin\\build-logic.js
+node .\\bin\\build-lookup.js
 
-node .\\bin\\build.js
+sh .\\bin\\build
+
+c8 ava
+
 node .\\bin\\build-template.js
-
-ava
