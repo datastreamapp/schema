90d0255240e8c32c37bc696d098de3b8ef2a008e
will Farrell <willfarrell@proton.me>
2024-04-25 07:53:21 -0600

    feat: add in support for value i18n

---
+++ schemas/data/bin/build-locales.js
@@ -0,0 +1,47 @@
+import { readFile, writeFile } from "node:fs/promises";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+import { subset, sort } from "./build-lib.js";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const columns = [
+  // filters
+  "ActivityMediaName",
+  "ActivityGroupType",
+  //'CharacteristicGroup',
+  "CharacteristicName",
+  "MonitoringLocationType",
+  // display
+  "MethodSpeciation",
+  "ResultSampleFraction",
+];
+
+const setup = async (column) => {
+  const locales = await readFile(
+    join(__dirname, `../src/locales/${column}.json`)
+  ).then((res) => JSON.parse(res));
+  const allowedValues = await readFile(
+    join(__dirname, `../src/values/${column}.primary.json`)
+  ).then((res) => JSON.parse(res));
+
+  for (const value of allowedValues.enum) {
+    locales[value] ??= { en: value, fr: null };
+    if (!locales[value].en) {
+      console.warn(`${column}["${value}"].en missing`);
+    }
+    if (!locales[value].fr) {
+      console.warn(`${column}["${value}"].fr missing`);
+    }
+  }
+
+  await writeFile(
+    join(__dirname, `../src/locales/${column}.json`),
+    JSON.stringify(locales, null, 2),
+    {
+      encoding: "utf8",
+    }
+  );
+};
+
+await Promise.all(columns.map((column) => setup(column)));
