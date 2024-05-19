6cdc60855a7141571902e816b3ae274b81790e07
will Farrell <willfarrell@proton.me>
2024-05-19 15:44:47 -0600

    chore: refactor i18n format

--- schemas/data/bin/build-locales.js
+++ schemas/data/bin/build-locales.js
@@ -7,10 +7,10 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const columns = [
   "ActivityMediaName", // filter
-  "ActivityGroupType", // filter
   "ActivityType",
+  "ActivityGroupType", // filter
   "CharacteristicName", // filter
-  //'CharacteristicGroup', // filter
+  "CharacteristicGroup", // filter
   "MethodSpeciation", // app display
   "MonitoringLocationHorizontalCoordinateReferenceSystem",
   "MonitoringLocationType", // app filter
@@ -21,30 +21,35 @@ const columns = [
   "ResultStatusID",
   "ResultValueType",
   "SampleCollectionEquipmentName",
-  "MeasurementUnit",
+  //"MeasurementUnit",
 ];
 
 const setup = async (column) => {
   const locales = await readFile(
     join(__dirname, `../src/locales/${column}.json`)
-  ).then((res) => JSON.parse(res));
+  )
+    .then((res) => JSON.parse(res))
+    .catch((e) => ({}));
   const allowedValues = await readFile(
     join(__dirname, `../src/values/${column}.primary.json`)
   ).then((res) => JSON.parse(res));
 
+  locales.name ??= { en: column, fr: null };
+  locales.description ??= { en: null, fr: null };
+  locales.values ??= {};
   for (const value of allowedValues.enum) {
-    locales[value] ??= {};
-    locales[value]["_"] ||= value;
-    if (column !== "MeasurementUnit") {
-      locales[value].en ||= value;
-    }
-    locales[value].en ||= null;
-    locales[value].fr ||= null;
+    locales.values[value] ??= {
+      name: { en: value, fr: null },
+      description: { en: null, fr: null },
+    };
+    // if (column !== "MeasurementUnit") {
+    //   locales[value].en ||= value;
+    // }
 
-    if (!locales[value].en) {
+    if (!locales.values[value].name.en) {
       console.warn(`${column}["${value}"].en missing`);
     }
-    if (!locales[value].fr) {
+    if (!locales.values[value].name.fr) {
       console.warn(`${column}["${value}"].fr missing`);
     }
   }
